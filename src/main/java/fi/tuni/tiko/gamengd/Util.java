b7784e2facb655673693f238d1b77730096dc619
Viljami Pietarila <viljami.pietarila@gmail.com>
2019-11-18 12:49:14 +0200

    Add javadoc to Util class.

--- src/main/java/fi/tuni/tiko/gamengd/Util.java
+++ src/main/java/fi/tuni/tiko/gamengd/Util.java
@@ -2,7 +2,26 @@ package fi.tuni.tiko.gamengd;
 
 import javafx.scene.image.Image;
 
+/**
+ * Util class is colleciton of helpful methods.
+ *
+ * Util class holds collection of helpful methods for running the game.
+ * The methods try to make the game engine run painlessly and try to make
+ * errors obvious without crashing the game.
+ *
+ * @author Viljami Pietarila
+ * @version 2019.1118
+ */
 public class Util {
+    /**
+     * loadImage method creates an Image from a file.
+     *
+     * loadImage attempts to open an image file and create an Image object
+     * out of it. If it fails to load the file, it will create an Image object
+     * using an obvious error image.
+     * @param file filename of the picture
+     * @return Image created of the picture
+     */
     public static Image loadImage(String file) {
         Image image;
         try {
