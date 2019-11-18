d7de71a79a7f39e50879839cf66106c22248cfee
Viljami Pietarila <viljami.pietarila@gmail.com>
2019-11-18 12:56:31 +0200

    Add Furniture class.
    Add furniture class. It extends Entity.

---
+++ src/main/java/fi/tuni/tiko/gamengd/entity/Furniture.java
@@ -0,0 +1,23 @@
+package fi.tuni.tiko.gamengd.entity;
+
+import fi.tuni.tiko.gamengd.Sprite;
+
+/**
+ * Furniture class is an Entity within game.
+ *
+ * Furniture is a game element that is placed above floors and walls, but
+ * under Units. This could be literal furniture or something like torch that
+ * could be placed on top of a wall.
+ *
+ * @author Viljami Pietarila
+ * @version 2019.1118
+ */
+public class Furniture extends Entity {
+    public Furniture(Sprite sprite, int x, int y) {
+        super(sprite, x, y);
+    }
+
+    public Furniture(Sprite sprite) {
+        super(sprite);
+    }
+}
