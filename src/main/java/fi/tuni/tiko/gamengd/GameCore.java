49a286a4bd99b1af49e373e7a56beea7544d7ed7
Viljami Pietarila <viljami.pietarila@gmail.com>
2019-12-03 13:01:23 +0200

    Change things for release.

--- src/main/java/fi/tuni/tiko/gamengd/GameCore.java
+++ src/main/java/fi/tuni/tiko/gamengd/GameCore.java
@@ -36,6 +36,11 @@ public class GameCore extends Application {
     private TurnController turnController;
     private InputController inputController;
 
+    public static void main(String[] args) {
+        System.out.println("Author: Viljami Pietarila");
+        System.exit(0);
+    }
+
     @Override
     public void init() {
         System.out.println("This is GameCore::Init");
