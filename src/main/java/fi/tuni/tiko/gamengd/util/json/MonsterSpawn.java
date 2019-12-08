9fea11e1401ef2a34dbf6c2fb4b30d0566433bba
Viljami Pietarila <viljami.pietarila@gmail.com>
2019-12-08 21:07:32 +0200

    Write more javadoc.

--- src/main/java/fi/tuni/tiko/gamengd/util/json/MonsterSpawn.java
+++ src/main/java/fi/tuni/tiko/gamengd/util/json/MonsterSpawn.java
@@ -10,32 +10,67 @@ package fi.tuni.tiko.gamengd.util.json;
  * @version 2019.1208
  */
 public class MonsterSpawn {
+    /**
+     * Type of the monster.
+     */
     private String type;
+    /**
+     * x-coordinate of the spawn point.
+     */
     private int spawnPointX;
+    /**
+     * y-coordinate of the spawn point.
+     */
     private int spawnPointY;
+
+    /**
+     * Constructor for MonsterSpawn.
+     */
     public MonsterSpawn() {
     }
 
+    /**
+     * getter for type.
+     * @return type
+     */
     public String getType() {
         return type;
     }
 
+    /**
+     * setter for type.
+     * @param type new type
+     */
     public void setType(String type) {
         this.type = type;
     }
 
+    /**
+     * getter for spawnPointX.
+     * @return spawnPointX
+     */
     public int getSpawnPointX() {
         return spawnPointX;
     }
 
+    /**
+     * setter for spawnPointX.
+     * @param spawnPointX new spawnPointX
+     */
     public void setSpawnPointX(int spawnPointX) {
         this.spawnPointX = spawnPointX;
     }
-
+    /**
+     * getter for spawnPointY.
+     * @return spawnPointY
+     */
     public int getSpawnPointY() {
         return spawnPointY;
     }
-
+    /**
+     * setter for spawnPointY.
+     * @param spawnPointY new spawnPointY
+     */
     public void setSpawnPointY(int spawnPointY) {
         this.spawnPointY = spawnPointY;
     }
