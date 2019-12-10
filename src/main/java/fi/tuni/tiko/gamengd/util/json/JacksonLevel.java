ff7d18d27ff6356d74ff71c73a368c58dc3adbb1
Viljami Pietarila <viljami.pietarila@gmail.com>
2019-12-10 10:29:58 +0200

    Add player attributes to JacksonLevel.
    Add Player attributes to JacksonLevel and to the json file.

--- src/main/java/fi/tuni/tiko/gamengd/util/json/JacksonLevel.java
+++ src/main/java/fi/tuni/tiko/gamengd/util/json/JacksonLevel.java
@@ -6,6 +6,9 @@ public class JacksonLevel {
     private String map;
     private int playerSpawnX;
     private int playerSpawnY;
+    private int playerHitPoints;
+    private int playerAttack;
+    private int playerDefense;
     private ArrayList<MonsterSpawn> monsterSpawns;
 
     public JacksonLevel() {
@@ -33,6 +36,30 @@ public class JacksonLevel {
 
     public void setPlayerSpawnY(int playerSpawnY) {
         this.playerSpawnY = playerSpawnY;
+    }
+
+    public int getPlayerHitPoints() {
+        return playerHitPoints;
+    }
+
+    public void setPlayerHitPoints(int playerHitPoints) {
+        this.playerHitPoints = playerHitPoints;
+    }
+
+    public int getPlayerAttack() {
+        return playerAttack;
+    }
+
+    public void setPlayerAttack(int playerAttack) {
+        this.playerAttack = playerAttack;
+    }
+
+    public int getPlayerDefense() {
+        return playerDefense;
+    }
+
+    public void setPlayerDefense(int playerDefense) {
+        this.playerDefense = playerDefense;
     }
 
     public ArrayList<MonsterSpawn> getMonsterSpawns() {
