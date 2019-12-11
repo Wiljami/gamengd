af6c627f56e04e8b6b88142ab61ed8e17f61d517
Viljami Pietarila <viljami.pietarila@gmail.com>
2019-12-11 16:24:51 +0200

    Begin work on better JacksonMap.

--- src/main/java/fi/tuni/tiko/gamengd/util/json/JacksonMap.java
+++ src/main/java/fi/tuni/tiko/gamengd/util/json/JacksonMap.java
@@ -1,9 +1,11 @@
 package fi.tuni.tiko.gamengd.util.json;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.util.ArrayList;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class JacksonMap {
     @JsonProperty("compressionlevel")
     private int compressionLevel;
