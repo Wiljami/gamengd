2f0f0ddd7a74b71a2a4522baca5aaef499da66dc
Viljami Pietarila <viljami.pietarila@gmail.com>
2019-12-15 02:02:12 +0200

    Refactor UI code.
    Refactor the arrow UI element to its own class. Rename the corner arrow to
    diagonal.

---
+++ src/main/java/fi/tuni/tiko/gamengd/ui/ArrowPad.java
@@ -0,0 +1,67 @@
+package fi.tuni.tiko.gamengd.ui;
+
+import fi.tuni.tiko.gamengd.controller.input.InputController;
+import fi.tuni.tiko.gamengd.controller.input.InputEvent;
+import fi.tuni.tiko.gamengd.util.ImageLoader;
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
+
+public class ArrowPad extends GridPane {
+    private static final String ARROWDIAGONAL = "arrowDiagonal.png";
+    private static final String ARROWSTRAIGHT = "arrowStraight.png";
+
+    public ArrowPad(InputController inputController) {
+        Button nw = movementButton(buttonImage(270, false));
+        nw.setOnAction(event -> inputController.receiveInputEvent(new InputEvent("button:NW")));
+        Button n = movementButton(buttonImage(0, true));
+        n.setOnAction(event -> inputController.receiveInputEvent(new InputEvent("button:N")));
+        Button ne = movementButton(buttonImage(0, false));
+        ne.setOnAction(event -> inputController.receiveInputEvent(new InputEvent("button:NE")));
+        Button w = movementButton(buttonImage(270, true));
+        w.setOnAction(event -> inputController.receiveInputEvent(new InputEvent("button:W")));
+        Button none = movementButton(new ImageView(ImageLoader.loadImage("square.png")));
+        none.setOnAction(event -> inputController.receiveInputEvent(new InputEvent("button:NONE")));
+        Button e = movementButton(buttonImage(90, true));
+        e.setOnAction(event -> inputController.receiveInputEvent(new InputEvent("button:E")));
+        Button sw = movementButton(buttonImage(180, false));
+        sw.setOnAction(event -> inputController.receiveInputEvent(new InputEvent("button:SW")));
+        Button s = movementButton(buttonImage(180, true));
+        s.setOnAction(event -> inputController.receiveInputEvent(new InputEvent("button:S")));
+        Button se = movementButton(buttonImage(90, false));
+        se.setOnAction(event -> inputController.receiveInputEvent(new InputEvent("button:SE")));
+
+        add(nw, 0,0);
+        add(n, 1,0);
+        add(ne, 2,0);
+        add(w, 0,1);
+        add(none, 1,1);
+        add(e, 2,1);
+        add(sw, 0,2);
+        add(s, 1,2);
+        add(se, 2,2);
+
+        setAlignment(Pos.BASELINE_CENTER);
+    }
+
+    private Button movementButton(ImageView image) {
+        Button b = new Button("", image);
+        b.setFocusTraversable(false);
+        return b;
+    }
+
+    private ImageView buttonImage (double rotation, boolean isStraight) {
+        Image image;
+        if (isStraight) {
+            image = ImageLoader.loadImage(ARROWSTRAIGHT);
+        } else {
+            image = ImageLoader.loadImage(ARROWDIAGONAL);
+        }
+
+        ImageView imageView = new ImageView(image);
+        imageView.setRotate(rotation);
+        return imageView;
+    }
+}
--- src/main/java/fi/tuni/tiko/gamengd/ui/UI.java
+++ src/main/java/fi/tuni/tiko/gamengd/ui/UI.java
@@ -10,6 +10,7 @@ import javafx.scene.control.MenuBar;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
+import javafx.scene.shape.Arc;
 
 public class UI extends BorderPane {
     private InputController inputController;
@@ -37,60 +38,9 @@ public class UI extends BorderPane {
         return topBar;
     }
 
-    private Button movementButton(ImageView image) {
-        Button b = new Button("", image);
-        b.setFocusTraversable(false);
-        return b;
-    }
-
     private Pane rightColumn() {
-        GridPane pane = new GridPane();
-        Button nw = movementButton(buttonImage(270, false));
-        nw.setOnAction(event -> inputController.receiveInputEvent(new InputEvent("button:NW")));
-        Button n = movementButton(buttonImage(0, true));
-        n.setOnAction(event -> inputController.receiveInputEvent(new InputEvent("button:N")));
-        Button ne = movementButton(buttonImage(0, false));
-        ne.setOnAction(event -> inputController.receiveInputEvent(new InputEvent("button:NE")));
-        Button w = movementButton(buttonImage(270, true));
-        w.setOnAction(event -> inputController.receiveInputEvent(new InputEvent("button:W")));
-        Button none = movementButton(new ImageView(ImageLoader.loadImage("square.png")));
-        none.setOnAction(event -> inputController.receiveInputEvent(new InputEvent("button:NONE")));
-        Button e = movementButton(buttonImage(90, true));
-        e.setOnAction(event -> inputController.receiveInputEvent(new InputEvent("button:E")));
-        Button sw = movementButton(buttonImage(180, false));
-        sw.setOnAction(event -> inputController.receiveInputEvent(new InputEvent("button:SW")));
-        Button s = movementButton(buttonImage(180, true));
-        s.setOnAction(event -> inputController.receiveInputEvent(new InputEvent("button:S")));
-        Button se = movementButton(buttonImage(90, false));
-        se.setOnAction(event -> inputController.receiveInputEvent(new InputEvent("button:SE")));
-
-        pane.add(nw, 0,0);
-        pane.add(n, 1,0);
-        pane.add(ne, 2,0);
-        pane.add(w, 0,1);
-        pane.add(none, 1,1);
-        pane.add(e, 2,1);
-        pane.add(sw, 0,2);
-        pane.add(s, 1,2);
-        pane.add(se, 2,2);
-
-        pane.setAlignment(Pos.BASELINE_CENTER);
-
-        HBox rightColumn = new HBox(pane);
+        HBox rightColumn = new HBox(new ArrowPad(inputController));
         return rightColumn;
-    }
-
-    private ImageView buttonImage (double rotation, boolean isStraight) {
-        Image image;
-        if (isStraight) {
-            image = ImageLoader.loadImage("arrowStraight.png");
-        } else {
-            image = ImageLoader.loadImage("arrowCorner.png");
-        }
-
-        ImageView imageView = new ImageView(image);
-        imageView.setRotate(rotation);
-        return imageView;
     }
 
     private VBox leftColumn() {
