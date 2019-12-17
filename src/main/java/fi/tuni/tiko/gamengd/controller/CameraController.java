2f12e3111ca72d1efad728bc56c2659b39b04f1c
Viljami Pietarila <viljami.pietarila@gmail.com>
2019-12-17 12:43:53 +0200

    More javadocs.

--- src/main/java/fi/tuni/tiko/gamengd/controller/CameraController.java
+++ src/main/java/fi/tuni/tiko/gamengd/controller/CameraController.java
@@ -20,18 +20,53 @@ import javafx.scene.layout.CornerRadii;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Popup;
-
 import java.util.HashMap;
 
+/**
+ * CameraController controls what is shown on the canvas.
+ *
+ * CameraController calculates what game elements are actually shown on the
+ * canvas. It is responsible for making sure that no Sprites outside the
+ * canvas are being drawn for no reason.
+ *
+ * @author Viljami Pietarila
+ * @version 2019.1217
+ */
 public class CameraController implements CommandTarget, TurnListener {
+    /**
+     * x-coordinate of the center point of the camera.
+     */
     private double x;
+    /**
+     * y-coordinate of the center point of the camera.
+     */
     private double y;
+    /**
+     * boolean wether the camera has been changed.
+     */
     private boolean cameraChanged;
+    /**
+     * size of a game tile in pixels.
+     */
     private double tileSize = 50;
+    /**
+     * Reference to the game canvas.
+     */
     private Canvas canvas;
+    /**
+     * Reference to the spriteController.
+     */
     private SpriteController spriteController;
+    /**
+     * Reference to the current Level.
+     */
     private Level level;
 
+    /**
+     * CameraController constructor.
+     * @param canvas canvas to be controlled
+     * @param spriteController spriteController reference
+     */
     public CameraController(Canvas canvas, SpriteController spriteController) {
         this.canvas = canvas;
         this.spriteController = spriteController;
@@ -42,6 +77,9 @@ public class CameraController implements CommandTarget, TurnListener {
         this.canvas.heightProperty().addListener(canvasSizeListener);
     }
 
+    /**
+     * updateSprites updates what Sprites are displayed on the canvas.
+     */
     public void updateSprites() {
 
         double centerSpriteX = canvas.getWidth() / 2 - (tileSize / 2);
@@ -53,6 +91,12 @@ public class CameraController implements CommandTarget, TurnListener {
         setCameraChanged(false);
     }
 
+    /**
+     * updateUnitSprites adds Units on the SpriteController.
+     *
+     * @param centerSpriteX x-coordinate of the center tile
+     * @param centerSpriteY y-coordinate of the center tile
+     */
     private void updateUnitSprites(double centerSpriteX, double centerSpriteY) {
         for (Unit unit : level.getUnits()) {
             Sprite s = unit.getSprite();
@@ -64,6 +108,16 @@ public class CameraController implements CommandTarget, TurnListener {
         }
     }
 
+    /**
+     * updateTileSprites determines what Tiles are displayed on the canvas.
+     *
+     * updateTileSprites calculates using the canvas size and the tileSize
+     * which Tiles are actually within the canvas. It then adds those tiles to
+     * the spriteController.
+     *
+     * @param centerSpriteX x-coordinate of the center tile
+     * @param centerSpriteY y-coordinate of the center tile
+     */
     private void updateTileSprites(double centerSpriteX, double centerSpriteY) {
         int horizontalTiles = (int) Math.ceil(canvas.getWidth() / tileSize) + 1;
         if (horizontalTiles % 2 == 0) horizontalTiles++;
@@ -107,6 +161,10 @@ public class CameraController implements CommandTarget, TurnListener {
         if (Config.toolTips) addToolTips(toolTips);
     }
 
+    /**
+     * addTooltips creates a toolTip popup for the mouse.
+     * @param tooltips HashMap of all the toolTips within the canvas.
+     */
     private void addToolTips(HashMap<Rectangle, String> tooltips) {
         Popup popup = new Popup();
         Label label = new Label("");
@@ -130,33 +188,64 @@ public class CameraController implements CommandTarget, TurnListener {
         });
     }
 
+    /**
+     * isCameraChanged.
+     * @return boolean cameraChanged.
+     */
     public boolean isCameraChanged() {
         return cameraChanged;
     }
 
+    /**
+     * setter for x.
+     * @param x new x
+     */
     public void setX(double x) {
         this.x = x;
         setCameraChanged(true);
     }
 
+    /**
+     * getter for x.
+     * @return x
+     */
     public double getX() {
         return x;
     }
 
+    /**
+     * setter for y.
+     * @param y new y
+     */
     public void setY(double y) {
         this.y = y;
         setCameraChanged(true);
     }
 
+    /**
+     * getter for y.
+     * @return y
+     */
+    public double getY() {
+        return y;
+    }
+
+    /**
+     * setter for x and y.
+     * @param x new x
+     * @param y new y
+     */
     public void setXY(double x, double y) {
         setX(x);
         setY(y);
     }
 
-    public double getY() {
-        return y;
-    }
-
+    /**
+     * setter for tileSize.
+     *
+     * The setter clamps the tileSize between 10 and 100.
+     * @param tileSize new tileSize.
+     */
     public void setTileSize(double tileSize) {
         if (tileSize < 10) tileSize = 10;
         if (tileSize > 100) tileSize = 100;
@@ -164,22 +253,40 @@ public class CameraController implements CommandTarget, TurnListener {
         setCameraChanged(true);
     }
 
+    /**
+     * getter for tileSize.
+     * @return tileSize
+     */
     public double getTileSize() {
         return tileSize;
     }
 
+    /**
+     * setter for level.
+     * @param level new level
+     */
     public void setLevel(Level level) {
         this.level = level;
     }
 
+    /**
+     * getter for level.
+     * @return level
+     */
     public Level getLevel() {
         return level;
     }
 
+    /**
+     * zoomIn zooms by changing the tileSize by 2%.
+     */
     private void zoomIn() {
         setTileSize(getTileSize() * 1.02);
     }
 
+    /**
+     * zoomOut zooms by changing the tileSize by 2%.
+     */
     private void zoomOut() {
         setTileSize(getTileSize() * 0.98);
     }
@@ -188,12 +295,23 @@ public class CameraController implements CommandTarget, TurnListener {
         this.cameraChanged = cameraChanged;
     }
 
+    /**
+     * receiveCommand is a method from CommandTarget interface.
+     *
+     * receiveCommand checks if the message received is either of the
+     * zoom messages.
+     * @param message String message from the Command.
+     */
     @Override
     public void receiveCommand(String message) {
         if(message.equals("ZOOMIN")) zoomIn();
         else if(message.equals("ZOOMOUT")) zoomOut();
     }
 
+    /**
+     * inform is a method from TurnListener interface.
+     * @param turnInfo info of the current turn.
+     */
     @Override
     public void inform(TurnInfo turnInfo) {
         setCameraChanged(true);
--- src/main/java/fi/tuni/tiko/gamengd/controller/SpriteController.java
+++ src/main/java/fi/tuni/tiko/gamengd/controller/SpriteController.java
@@ -6,13 +6,44 @@ import javafx.scene.canvas.GraphicsContext;
 import java.util.LinkedList;
 import java.util.List;
 
+/**
+ * SpriteController holds list of Sprites that are drawn.
+ *
+ * SpriteController's lists of Sprites are in turn handled by the
+ * CameraController which calculates which Sprites actually are within the
+ * canvas.
+ *
+ * @author Viljami Pietarila
+ * @version 2019.1217
+ */
 public class SpriteController {
 
+    /**
+     * List of Tile Sprites.
+     */
     private List<Sprite> tileSprites = new LinkedList<>();
+    /**
+     * List of Wall Sprites.
+     */
     private List<Sprite> wallSprites = new LinkedList<>();
+    /**
+     * List of Furniture Sprites.
+     */
     private List<Sprite> furnSprites = new LinkedList<>();
+    /**
+     * List of Unit Sprites.
+     */
     private List<Sprite> unitSprites = new LinkedList<>();
 
+    /**
+     * render method goes through all the lists and renders their Sprites.
+     *
+     * render method calls the renderSprites in order. The order is important
+     * as the first rendered Sprites go under the ones rendered after.
+     * @param gc GraphicContext
+     * @param time time since last frame
+     * @param tileSize size of the tile
+     */
     public void render(GraphicsContext gc, double time, double tileSize) {
         renderSprites(gc, time, tileSize, tileSprites);
         renderSprites(gc, time, tileSize, wallSprites);
@@ -20,6 +51,13 @@ public class SpriteController {
         renderSprites(gc, time, tileSize, unitSprites);
     }
 
+    /**
+     * renderSprites renders a List of Sprites.
+     * @param gc GraphicsContext
+     * @param time time since last frame
+     * @param tileSize size of the tiles
+     * @param sprites list of Sprites
+     */
     private void renderSprites(GraphicsContext gc, double time,
                                double tileSize, List<Sprite> sprites) {
         for (Sprite s : sprites) {
@@ -28,22 +66,41 @@ public class SpriteController {
         }
     }
 
+    /**
+     * addFloorSprite adds a new Sprite to the tileSprites.
+     * @param sprite Sprite to be added
+     */
     public void addFloorSprite(Sprite sprite) {
         tileSprites.add(sprite);
     }
 
+    /**
+     * addWallSprite adds a new Sprite to the wallSprites.
+     * @param sprite Sprite to be added
+     */
     public void addWallSprite(Sprite sprite) {
         wallSprites.add(sprite);
     }
 
+    /**
+     * addFurnSprite adds a new Sprite to the furnSprites.
+     * @param sprite Sprite to be added
+     */
     public void addFurnSprite(Sprite sprite) {
         furnSprites.add(sprite);
     }
 
+    /**
+     * addUnitSprite adds a new Sprite to the unitSprites.
+     * @param sprite Sprite to be added
+     */
     public void addUnitSprite(Sprite sprite) {
         unitSprites.add(sprite);
     }
 
+    /**
+     * clear method clears all the Lists of the Sprites.
+     */
     public void clear() {
         tileSprites.clear();
         wallSprites.clear();
