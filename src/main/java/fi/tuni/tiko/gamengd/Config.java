3a6e46b5f36bb3957a06134decdc7bd23eae255d
Viljami Pietarila <viljami.pietarila@gmail.com>
2019-12-17 15:20:57 +0200

    Remove final keyword from Config.

--- src/main/java/fi/tuni/tiko/gamengd/Config.java
+++ src/main/java/fi/tuni/tiko/gamengd/Config.java
@@ -1,13 +1,13 @@
 package fi.tuni.tiko.gamengd;
 
 public class Config {
-    public static final double resolutionX = 1200;
-    public static final double resolutionY = 800;
-    public static final double minResolutionX = 600;
-    public static final double minResolutionY = 400;
-    public static final boolean fullScreen = false;
-    public static final String defaultGame = "game.json";
+    public static double resolutionX = 1200;
+    public static double resolutionY = 800;
+    public static double minResolutionX = 600;
+    public static double minResolutionY = 400;
+    public static boolean fullScreen = false;
+    public static String defaultGame = "game.json";
 
-    public static final boolean mouseControl = true;
-    public static final boolean toolTips = true;
+    public static boolean mouseControl = true;
+    public static boolean toolTips = true;
 }
