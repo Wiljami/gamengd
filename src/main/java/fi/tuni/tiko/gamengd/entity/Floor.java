55f0c9ad8fd188852e91c2d037923b3b9eb19e42
Viljami Pietarila <viljami.pietarila@gmail.com>
2019-12-17 13:06:16 +0200

    More javadocs.

--- src/main/java/fi/tuni/tiko/gamengd/entity/Floor.java
+++ src/main/java/fi/tuni/tiko/gamengd/entity/Floor.java
@@ -5,17 +5,38 @@ import fi.tuni.tiko.gamengd.util.ImageLoader;
 import javafx.scene.image.Image;
 import javafx.scene.image.WritableImage;
 
+/**
+ * Floor is the bottom element on a Tile.
+ *
+ * @author Viljami Pietarila
+ * @version 2019.1217
+ */
 public class Floor extends Entity {
+    /**
+     * Image of an empty Floor.
+     */
     private static Image emptyFloor;
 
+    /**
+     * Floor's static setup method setups the emptyFloor image.
+     */
     public static void setup() {
         emptyFloor = ImageLoader.loadImage("voidtile.png");
     }
 
+    /**
+     * Floor constructor.
+     * @param image WritableImage of the floor.
+     */
     public Floor(WritableImage image) {
         super(new Sprite(image));
     }
 
+    /**
+     * Floor constructor.
+     *
+     * A floor constructor without WritableImage will use the emptyFloor.
+     */
     public Floor() {
         super(new Sprite(emptyFloor));
     }
