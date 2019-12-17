c2ec012544ac63aa067c59ca4171b95d82984806
Viljami Pietarila <viljami.pietarila@gmail.com>
2019-12-17 12:20:01 +0200

    More javadocs.

--- src/main/java/fi/tuni/tiko/gamengd/controller/ui/UIController.java
+++ src/main/java/fi/tuni/tiko/gamengd/controller/ui/UIController.java
@@ -4,25 +4,58 @@ import fi.tuni.tiko.gamengd.ui.GameLog;
 
 import java.util.ArrayList;
 
+/**
+ * UIController controls flow of info from game to the UI elements.
+ *
+ * @author Viljami Pietarila
+ * @version 2019.1217
+ */
 public class UIController {
+    /**
+     * List of uiListeners.
+     */
     private ArrayList<UIListener> uiListeners;
+
+    /**
+     * Reference to the gameLog.
+     */
     private GameLog gameLog;
+
+    /**
+     * UIController constructor.
+     */
     public UIController() {
         uiListeners = new ArrayList<>();
     }
 
+    /**
+     * addUIlistener adds an UI listener to the controller.
+     * @param listener UIListener to be added.
+     */
     public void addUIListener (UIListener listener) {
         uiListeners.add(listener);
     }
 
+    /**
+     * addGameLog creates a reference to the gameLog.
+     * @param gameLog GameLog of the game.
+     */
     public void addGameLog(GameLog gameLog) {
         this.gameLog = gameLog;
     }
 
+    /**
+     * updateGameLog lets the gameLog know of a new message to display.
+     * @param message message to display.
+     */
     public void updateGameLog(String message) {
         gameLog.updateGameLog(message);
     }
 
+    /**
+     * trigger goes through the uiListeners and sends given message to each.
+     * @param message message to the uiListeners
+     */
     public void trigger (String message) {
         for (UIListener uiListener : uiListeners) {
             uiListener.triggerUIListener();
--- src/main/java/fi/tuni/tiko/gamengd/controller/ui/UIListener.java
+++ src/main/java/fi/tuni/tiko/gamengd/controller/ui/UIListener.java
@@ -1,5 +1,14 @@
 package fi.tuni.tiko.gamengd.controller.ui;
 
+/**
+ * UIListener interface lets object listen to changes to ui.
+ *
+ * @author Viljami Pietarila
+ * @version 2019.1217
+ */
 public interface UIListener {
+    /**
+     * UILIst
+     */
     void triggerUIListener();
 }
