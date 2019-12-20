f8d7019d9d9685afad578543fb317d3c7ea40e1c
Viljami Pietarila <viljami.pietarila@gmail.com>
2019-12-20 08:56:56 +0200

    Add javaDocs.

--- src/main/java/fi/tuni/tiko/gamengd/ui/UI.java
+++ src/main/java/fi/tuni/tiko/gamengd/ui/UI.java
@@ -6,15 +6,42 @@ import fi.tuni.tiko.gamengd.controller.input.InputController;
 import fi.tuni.tiko.gamengd.controller.ui.UIController;
 import fi.tuni.tiko.gamengd.entity.Player;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 
+/**
+ * UI is the collection of all the displayed UI elements.
+ *
+ * It extends BorderPane and sets each of BorderPane's areas to contain
+ * ui elements.
+ * @author Viljami Pietarila
+ * @version 2019.1220
+ */
 public class UI extends BorderPane {
+    /**
+     * Reference to inputController
+     */
     private InputController inputController;
+    /**
+     * Reference to uiController
+     */
     private UIController uiController;
+    /**
+     * Reference to gameCore
+     */
     private GameCore gameCore;
+    /**
+     * Reference to player
+     */
     private Player player;
 
+    /**
+     * UI constructor.
+     *
+     * UI constructor sets up the different UI elements within itself.
+     * @param gameCore Reference to the gameCore
+     */
     public UI(GameCore gameCore) {
         this.inputController = gameCore.getInputController();
         this.uiController = gameCore.getUiController();
@@ -27,19 +54,36 @@ public class UI extends BorderPane {
         setLeft(leftColumn());
     }
 
-
-    private VBox topBar() {
-        VBox topBar = new VBox(new GameMenuBar(player, gameCore));
+    /**
+     * topBar is the ui element set to the top of the UI.
+     *
+     * topBar holds the GameMenuBar.
+     * @return topBar Pane
+     */
+    private Pane topBar() {
+        HBox topBar = new HBox(new GameMenuBar(player, gameCore));
         return topBar;
     }
 
-    private VBox bottomBar() {
+    /**
+     * bottomBar is the ui element set to the bottom of the UI.
+     *
+     * bottomBar holds the gameLog.
+     * @return bottomBar Pane
+     */
+    private Pane bottomBar() {
         GameLog gameLog = new GameLog();
         uiController.addGameLog(gameLog);
-        VBox topBar = new VBox(gameLog);
-        return topBar;
+        HBox bottomBar = new HBox(gameLog);
+        return bottomBar;
     }
 
+    /**
+     * rightColumn is the ui element set to the right side of the UI.
+     *
+     * rightColumn holds the playerDisplay and the arrowPad.
+     * @return rightColumn Pane
+     */
     private Pane rightColumn() {
         PlayerDisplay playerDisplay = new PlayerDisplay(player);
         uiController.addUIListener(playerDisplay);
@@ -51,7 +95,13 @@ public class UI extends BorderPane {
         return rightColumn;
     }
 
-    private VBox leftColumn() {
+    /**
+     * leftColumn is the ui element set to the left of the UI.
+     *
+     * leftColumn holds nothing.
+     * @return leftColumn Pane
+     */
+    private Pane leftColumn() {
         VBox topBar = new VBox();
         return topBar;
     }
