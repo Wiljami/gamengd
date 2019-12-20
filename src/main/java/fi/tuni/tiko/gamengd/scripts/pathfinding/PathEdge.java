6c72d55564db3a3efb03f62fa28fd9847d1bd967
Viljami Pietarila <viljami.pietarila@gmail.com>
2019-12-20 08:23:00 +0200

    Add javaDocs.

--- src/main/java/fi/tuni/tiko/gamengd/scripts/pathfinding/PathEdge.java
+++ src/main/java/fi/tuni/tiko/gamengd/scripts/pathfinding/PathEdge.java
@@ -1,6 +1,15 @@
 package fi.tuni.tiko.gamengd.scripts.pathfinding;
 
+/**
+ * PathEdge is an edge on an object leading to another object for pathfinding.
+ *
+ * @author Viljami Pietarila
+ * @version 2019.1220
+ * @param <T> type of Object that makes up the Nodes.
+ */
 class PathEdge<T> {
-    //public int movementCost???
+    /**
+     * reference to the node this PathEdge belongs to.
+     */
     public PathNode<T> node;
 }
