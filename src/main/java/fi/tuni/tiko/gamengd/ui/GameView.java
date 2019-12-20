4fe91562214add730d85ad054c0eed18ac6a1613
Viljami Pietarila <viljami.pietarila@gmail.com>
2019-12-20 08:45:33 +0200

    Add javaDocs.

--- src/main/java/fi/tuni/tiko/gamengd/ui/GameView.java
+++ src/main/java/fi/tuni/tiko/gamengd/ui/GameView.java
@@ -3,8 +3,25 @@ package fi.tuni.tiko.gamengd.ui;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.layout.Pane;
 
+/**
+ * GameView is the displayed game elements in the middle of the UI.
+ *
+ * GameView holds a canvas and sets the canvas size according to the size
+ * of the window. Sets the canvas to automatically resize itself if the size
+ * changes.
+ *
+ * @author Viljami Pietarila
+ * @version 2019.1220
+ */
 public class GameView extends Pane {
+    /**
+     * the Canvas within the GameView
+     */
     private Canvas canvas;
+
+    /**
+     * GameView constructor.
+     */
     public GameView() {
         canvas = new Canvas();
         getChildren().add(canvas);
@@ -13,6 +30,10 @@ public class GameView extends Pane {
         canvas.heightProperty().bind(this.heightProperty());
     }
 
+    /**
+     * getter for canvas.
+     * @return canvas
+     */
     public Canvas getCanvas() {
         return canvas;
     }
