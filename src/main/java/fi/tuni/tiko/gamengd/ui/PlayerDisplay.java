4bc1e8474ee05286b357b0099e724c9bd4904653
Viljami Pietarila <viljami.pietarila@gmail.com>
2019-12-20 08:49:55 +0200

    Add javaDocs.

--- src/main/java/fi/tuni/tiko/gamengd/ui/PlayerDisplay.java
+++ src/main/java/fi/tuni/tiko/gamengd/ui/PlayerDisplay.java
@@ -8,16 +8,41 @@ import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
 
+/**
+ * PlayerDisplay is an UI element displaying the player stats.
+ *
+ * It implements UIListener to be able to receive changes that happen in the
+ * player stats and it extends GridPane as the base UI element.
+ * @author Viljami Pietarila
+ * @version 2019.1220
+ */
 public class PlayerDisplay extends GridPane implements UIListener {
+    /**
+     * Reference to the name Text field
+     */
     private Text name;
+    /**
+     * Reference to the hitPoints Text field
+     */
     private Text hitPoints;
+    /**
+     * Reference to the attack Text field
+     */
     private Text attack;
+    /**
+     * Reference to the defence Text field
+     */
     private Text defence;
+    /**
+     * Reference to the killCount Text field
+     */
     private Text killCount;
-    private Player player;
-    public PlayerDisplay(Player player) {
-        this.player = player;
 
+    /**
+     * PlayerDisplay constructor.
+     * @param player player whose stats we display.
+     */
+    public PlayerDisplay(Player player) {
         setHgap(10);
         setVgap(10);
         setPadding(new Insets(10, 10, 10, 10));
@@ -27,6 +52,9 @@ public class PlayerDisplay extends GridPane implements UIListener {
         triggerUIListener(player);
     }
 
+    /**
+     * setupChangingTexts adds the changing Texts to the PlayerDisplay.
+     */
     private void setupChangingTexts() {
         Font font = Font.font("Arial", 12);
 
@@ -48,6 +76,9 @@ public class PlayerDisplay extends GridPane implements UIListener {
         add(killCount, 1, 4);
     }
 
+    /**
+     * setupLabels adds the static texts to the PlayerDisplay.
+     */
     private void setupLabels() {
         Font font = Font.font("Arial", FontWeight.BOLD, 12);
 
@@ -69,6 +100,13 @@ public class PlayerDisplay extends GridPane implements UIListener {
         add(killCountText, 0, 4);
     }
 
+    /**
+     * triggerUIListener is a method from UIListener interface.
+     *
+     * Upon being called this method updates the values within the changing
+     * Texts in the PlayerDisplay.
+     * @param source Player whose stats are displayed
+     */
     @Override
     public void triggerUIListener(Player source) {
         name.setText(source.getName());
